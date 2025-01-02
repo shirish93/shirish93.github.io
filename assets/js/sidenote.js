d8396c69804e11abc0c52276270b37d2bf16df32
Shirish Pokharel <shirishkpokharel@gmail.com>
2025-01-01 16:44:06 -0800

    feat: add sidenote

---
+++ assets/js/sidenote.js
@@ -0,0 +1,123 @@
+/**
+ * @author: Kaushik Gopal
+ *
+ * A jQuery function that displays the footnotes
+ * on the side (sidenotes) for easier reading.
+ *
+ * This is as recommended by Edward Tufte's style sidenotes:
+ * https://edwardtufte.github.io/tufte-css/#sidenotes
+ *
+ * TODO:
+ *      - if two subsequent lines have long sidenotes
+ *        need to take care of the overlap properly and offset
+ **/
+(function () {
+    const $footnotes = $(".footnotes"),
+        sideNoteStartMargin = 12,
+        sideNoteMaxWidth = 280,
+        sideNoteMinWidth = 140;
+
+    $(window).on("load", function () {
+
+
+        // don't run this script if there aren't any footnotes
+        if ($footnotes.length < 1) {
+            return;
+        }
+
+        loadSideNotesFromFootnotes();
+
+        $(window).resize(function () {
+            // console.log(" XXX -- RESIZE -- XXX ");
+
+            // TODO: optimization if window width doesn't change that much
+            // const new_ww = $(".wrapper").outerWidth();
+            // if (new_ww === windowWidth) return;
+            // windowWidth = new_ww;
+
+            loadSideNotesFromFootnotes();
+        });
+    });
+
+    
+    function loadSideNotesFromFootnotes() {
+
+        const $postTitle = $(".posttitle"),
+            browserWidth = $(".main-content").width(),
+            startPosition = $postTitle.offset().left + $postTitle.outerWidth() + sideNoteStartMargin;
+
+        $(".sidenote").remove(); // remove any existing side notes to begin
+        $footnotes.show();  // previous resize could have hidden footnotes
+
+        //#region Should we even show sidenotes?
+
+        //#region there's no post-content
+        if ($postTitle.length < 1) {
+            return;
+        }
+        //#endregion
+
+        //#region there's no space for sidenotes
+        const availableSpaceForSideNote = browserWidth - startPosition;
+
+        console.log(" ---> availableSpaceForSideNote " + availableSpaceForSideNote);
+        console.log(" ---> sideNoteWidth [" + sideNoteMinWidth + " - " + sideNoteMaxWidth + "]");
+
+        // some magic I'm doing because this is broken.
+        if (availableSpaceForSideNote+200 < sideNoteMinWidth) {
+            return;
+        }
+        //#endregion
+
+        //#endregion
+
+        const $fnItems = $footnotes.find("ol li");
+
+        $("sup").each(function (index) {
+            const $footnoteHtml = $fnItems.eq(index).html();
+            createSideNote($(this), $footnoteHtml, startPosition);
+        });
+
+        $footnotes.hide();
+    }
+
+
+    function createSideNote(superscript, footnoteHtml, startPosition) {
+
+        // console.log(" ---> " + superscript.text() + " : " + footnoteHtml);
+
+        // construct side note <div>
+        let div = $(document.createElement('div'))
+            .html(footnoteHtml)
+            .addClass("sidenote");
+
+        const topPosition = superscript.offset();
+
+        div.css({
+            position: "absolute",
+            left: startPosition,
+            top: topPosition["top"],
+            minWidth: sideNoteMinWidth,
+            maxWidth: sideNoteMaxWidth,
+        });
+
+        if (startPosition > 420) {
+            superscript.hover(function () {
+                div.addClass("sidenote-hover");
+            }, function () {
+                div.removeClass("sidenote-hover");
+            });
+        } else {
+            div.addClass("sidenote-hover");
+        }
+
+        // console.log(" ---> ");
+
+        // attach side note <div>
+        $(document.body).append(div);
+    }
+
+})();
+
+
+
